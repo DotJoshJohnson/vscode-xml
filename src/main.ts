30e82227174f20ea72f99209de0b3524d7493a6d
Josh Johnson <josh@fyrelogic.com>
2015-11-22 01:48:40 -0500

    Debug Configs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 'use strict';
 
-import { ExtensionContext } from 'vscode';
+import { commands, ExtensionContext } from 'vscode';
+import { formatXml } from './features/xmlFormatting';
 
 export function activate(ctx: ExtensionContext) {
+	// check for update
+	//...
 	
+	// register pallete commands
+	ctx.subscriptions.push(commands.registerTextEditorCommand('xmltools.formatXml', formatXml));
 }
