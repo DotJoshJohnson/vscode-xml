351608a718b724ffeb6f93183d3eb4b49f227997
Josh Johnson <josh@fyrelogic.com>
2015-11-24 13:59:42 -0500

    Add ExtensionManifestReader

---
+++ src/utils/ManifestUtils.ts
@@ -0,0 +1,37 @@
+import { extensions, Extension } from 'vscode';
+
+export class ExtensionManifestReader {
+	constructor(extensionId: string) {
+		this._extension = extensions.getExtension(extensionId);
+	}
+	
+	private _extension: Extension<any>
+	
+	name: string;
+	version: string;
+	publisher: string;
+	displayName: string;
+	description: string;
+	categories: string[];
+	keywords: string[];
+	icon: string;
+	
+	private _checkPropertyExists(propertyName: string) {
+		for (let property in this) {
+			if (property === propertyName) return true;
+		}
+		
+		return false;
+	}
+	
+	refresh(): void {
+		let manifest: any = this._extension.packageJSON;
+		
+		for (let property in manifest)
+		{
+			if (this._checkPropertyExists(property)) {
+				this[property] = manifest[property];
+			}
+		}
+	}
+}
