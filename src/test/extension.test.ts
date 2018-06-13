69a7a9164bede4989d5644f84a8ca2f710667a2f
Josh Johnson <josh.johnson@leafyacre.com>
2018-06-12 20:11:52 -0400

    Add Failing Test
    Issue: #193

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -80,6 +80,10 @@ describe("V2XmlFormatter", () => {
             testFormatter(xmlFormatter, options, "issue-189");
         });
 
+        it("should not add extra line breaks before closing tags", () => {
+            testFormatter(xmlFormatter, options, "issue-193");
+        });
+
     });
 
 });
